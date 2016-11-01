d033dc0d433ea48e04163b15f6ed2d9c7637e257
Brandon McClinton <bmcclint@Brandons-Mac.mshome.net>
2016-11-01 11:41:01 -0500

    working on the queries

--- dbqueries.sql
+++ dbqueries.sql
@@ -1,35 +1,93 @@
 /* 1. List a company's workers by names*/
+/*EMPLOYMENT BROKEN*/
 select last_name, first_name
 from PERSON natural join EMPLOYMENT natural join JOB
 where comp_code = '1017145';
 
 /* 2. List a company's staff by salary in descending order.*/
-
-
+/*EMPLOYMENT IS BROKEBNT*/
+select person_code, last_name, first_name, pay_rate
+from person natural join employment natural join job
+where comp_code = '1001001' and pay_type = 'salary' and
+      start_date < CURRENT_DATE and
+      (end_date > CURRENT_DATE or end_date is null)
+order by(pay_rate) desc;
 
 /*3. List a company's labor cost (total salaries and wage rates by 1920 hours)
 in descending order. */
-
+with total_salary as (
+  select sum(pay_rate) as t_salary, comp_code from job natural join company
+  where pay_type = 'salary' and
+  (start_date < CURRENT_DATE) and
+  (end_date > CURRENT_DATE or end_date is null) group by comp_code),
+total_wages as (
+  select sum(pay_rate* 1920) as t_wages, comp_code from job natural join company
+  where pay_type = 'wages' and
+  (start_date < CURRENT_DATE) and
+  (end_date > CURRENT_DATE or end_date is null)
+  group by comp_code)
+select (t_salary + t_wages) as total_cost, comp_code from total_salary natural join total_wages
+order by total_cost desc;
 
 /*4. Find all the jobs a person is currently holding and worked in the past.*/
-
+select job_code
+from employment
+where person_code = '1047996' and start_date < CURRENT_DATE;
 
 /*5. List a person's knowledge skills in a readable format.*/
-
+select ks_name
+from person_ks natural join KNOWLEDGE_SKILLS where person_code = '1047996';
 
 /*6. list the skill gap of a worker between his/her job(s) and skill(s).*/
-
+/*BROKENT*/
+with person_current_jobs as (
+select job_code, jp_code
+from employment NATURAL JOIN job
+where person_code = '1047996' and
+start_date < CURRENT_DATE and
+(end_date > CURRENT_DATE or end_date is null) ) (select ks_code
+from person_current_jobs natural join job_skill union
+select ks_code
+from person_current_jobs natural join jp_skill) minus
+select ks_code
+from person_ks
+where person_code = '1047996';
 
 /*7. List the required knowledge skills of a job profile in a readable format.*/
-
+select ks_name
+from job_profile natural join KNOWLEDGE_SKILLS
+where jp_code = '100';
 
 /*8. List a person's missing knowledge skills for a specific job in a readable
 format.*/
-
+select ks_name
+from knowledge_skills natural join
+  ((select ks_code
+    from knowledge_skills natural join job
+    where job_code = '2001001')
+    union
+    select ks_code
+    from jp_skill natural join job
+    where job_code = '2001001')
+    minus
+   (select ks_code
+    from person_ks
+    where person_code = '1014890');
 
 /*9. List the course (course id and title) that each alone teaches all 
 the missing knowledge skill for a person to pursue a specicific job.*/
-
+/*BROKEN*/
+with needed_skills as (
+  select ks_code
+  from jp_skill natural join job_skill where jp_code = '100')
+select course_code
+from COURSE_SKILL C where not exists 
+  (select *
+  from needed_skills
+minus
+  select ks_code
+  from course_skill
+  where course_skill.course_code = C.course_code);
 
 /*10. Suppose the skill gap for a  worker and the requirement of a desired job 
 can be covered byone course. Find the "quickest" solution for this worker. Show 
