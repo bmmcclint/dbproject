bf22e5dc9c03b09dbe646800f8410eb177fd1310
Brandon McClinton <bmcclint@Brandons-Mac.mshome.net>
2016-11-03 11:57:04 -0500

    first few queries fixed

--- dbqueries.sql
+++ dbqueries.sql
@@ -1,51 +1,52 @@
 /* 1. List a company's workers by names*/
-/*EMPLOYMENT BROKEN*/
 select last_name, first_name
-from PERSON natural join EMPLOYMENT natural join JOB
-where comp_code = '1017145';
+from person inner join employment on person.PERSON_CODE = employment.PERSON_CODE inner join job on employment.JOB_CODE = job.JOB_CODE  
+where comp_code = '1001001';
 
 /* 2. List a company's staff by salary in descending order.*/
-/*EMPLOYMENT IS BROKEBNT*/
-select person_code, last_name, first_name, pay_rate
-from person natural join employment natural join job
-where comp_code = '1001001' and pay_type = 'salary' and
-      start_date < CURRENT_DATE and
-      (end_date > CURRENT_DATE or end_date is null)
+select last_name, first_name, pay_rate
+from person inner join employment on person.PERSON_CODE = employment.PERSON_CODE inner join job on employment.JOB_CODE = job.JOB_CODE
+where comp_code = '1001001' and pay_type = 'salary' 
 order by(pay_rate) desc;
 
 /*3. List a company's labor cost (total salaries and wage rates by 1920 hours)
 in descending order. */
-with total_salary as (
-  select sum(pay_rate) as t_salary, comp_code from job natural join company
-  where pay_type = 'salary' and
-  (start_date < CURRENT_DATE) and
-  (end_date > CURRENT_DATE or end_date is null) group by comp_code),
-total_wages as (
-  select sum(pay_rate* 1920) as t_wages, comp_code from job natural join company
-  where pay_type = 'wages' and
-  (start_date < CURRENT_DATE) and
-  (end_date > CURRENT_DATE or end_date is null)
-  group by comp_code)
-select (t_salary + t_wages) as total_cost, comp_code from total_salary natural join total_wages
+with salary as (
+  select comp_code, sum(pay_rate) worker_salary
+  from job inner join employment on job.JOB_CODE = employment.JOB_CODE
+  where pay_type = 'salary'
+  group by (job.COMP_CODE)),
+
+wage as (
+  select comp_code, sum(pay_rate*1920) worker_salary
+  from job inner join employment on job.JOB_CODE = employment.JOB_CODE
+  where pay_type = 'wages'
+  group by (job.COMP_CODE)),
+
+cost as 
+  ((select * from salary) union (select * from wage))
+select comp_code, sum(worker_salary) as total_cost
+from cost
+group by comp_code
 order by total_cost desc;
 
 /*4. Find all the jobs a person is currently holding and worked in the past.*/
 select job_code
-from employment
-where person_code = '1047996' and start_date < CURRENT_DATE;
+from employment natural join person 
+where person_code = '1018256';
 
 /*5. List a person's knowledge skills in a readable format.*/
 select ks_name
-from person_ks natural join KNOWLEDGE_SKILLS where person_code = '1047996';
+from person_ks natural join knowledge_skills
+where person_code = '1536512';
 
 /*6. list the skill gap of a worker between his/her job(s) and skill(s).*/
 /*BROKENT*/
 with person_current_jobs as (
 select job_code, jp_code
-from employment NATURAL JOIN job
-where person_code = '1047996' and
-start_date < CURRENT_DATE and
-(end_date > CURRENT_DATE or end_date is null) ) (select ks_code
+from employment natural join job 
+where person_code = '1047996') 
+(select ks_code
 from person_current_jobs natural join job_skill union
 select ks_code
 from person_current_jobs natural join jp_skill) minus
@@ -70,24 +71,57 @@ from knowledge_skills natural join
     from jp_skill natural join job
     where job_code = '2001001')
     minus
-   (select ks_code
+    (select ks_code
     from person_ks
-    where person_code = '1014890');
+where person_code = '1014890');
 
 /*9. List the course (course id and title) that each alone teaches all 
 the missing knowledge skill for a person to pursue a specicific job.*/
-/*BROKEN*/
 with needed_skills as (
-  select ks_code
-  from jp_skill natural join job_skill where jp_code = '100')
-select course_code
-from COURSE_SKILL C where not exists 
-  (select *
-  from needed_skills
+select ks_code
+from job_skill natural join job where jp_code = 101
 minus
-  select ks_code
-  from course_skill
-  where course_skill.course_code = C.course_code);
+select ks_code
+from person_ks
+where person_code = '1047996'
+),
+course_sets as (
+select course_code as A, null as B, null as C
+from course
+union
+select A.course_code as A, B.course_code as B, null as C
+from course A, course B
+where A.course_code < B.course_code
+union
+select A.course_code as A, B.course_code as B, C.course_code as C
+from course A, course B, course C
+where A.course_code < B.course_code and B.course_code < C.course_code )
+,
+course_set_skills as (
+select A, B, C, ks_code
+from course_skill, course_sets
+where course_sets.A = course_skill.course_code or
+course_sets.B = course_skill.course_code or
+course_sets.C = course_skill.course_code ),
+satisfactory_sets as (
+select *
+from (
+(select A, B, C from course_set_skills) minus
+(select A, B, C
+from (select A, B, C, ks_code from(select A, B, C
+from course_set_skills),
+(select *
+from needed_skills) minus
+select A, B, C, ks_code from course_set_skills) ) ) ) ,
+element_count as ( select A, B, C ,
+case
+when B is null then 1 when C is null then 2
+else 3
+end as number_of_courses
+from satisfactory_sets)
+select A, B, C, number_of_courses
+from element_count
+where number_of_courses = (select min(number_of_courses) from element_count);
 
 /*10. Suppose the skill gap for a  worker and the requirement of a desired job 
 can be covered byone course. Find the "quickest" solution for this worker. Show 
