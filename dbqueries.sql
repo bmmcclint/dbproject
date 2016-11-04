6c7e14669b28fcac5452967ebcf23ab92b11d4b6
Brandon McClinton <bmcclint@Brandons-Mac.mshome.net>
2016-11-04 03:13:49 -0500

    query no. 16 done

--- dbqueries.sql
+++ dbqueries.sql
@@ -156,10 +156,32 @@ where not exists (
 solution is to find a person who is almost qualified to the job. Make a 
 "missing-one" list that lists people who miss only one skill for a specified 
 job profile.*/
+with skill_codes as (
+  select ks_code
+  from jp_skill
+  where jp_code = '666'),
 
+missing_one (person_code, num_missing) as (
+select person_code, count(ks_code)
+  from person P,
+    (select *
+    from skill_codes) SC
+    where SC.ks_code in (
+      select *
+      from skill_codes
+        minus
+      select ks_code
+      from person_skill 
+      where person_code = P.person_code)
+    group by person_code)
+    
+select person_code, num_missing
+from missing_one
+where num_missing = 1
+order by num_missing desc;
 
 /*17. List the skillID and the number of people in the missing-one list for a 
-given job profile in the ascendingorder of the people counts.*/
+given job profile in the ascending order of the people counts.*/
 
 
 /*18. Suppose there is a new job profile that has nobody qualified. list the 
